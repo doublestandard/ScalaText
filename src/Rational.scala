0e8d3c5d8927c5bef6ea725137955976c05a5140
doublestandard <miyamegu@gmail.com>
2017-06-15 14:35:24 +0900

    2017/06/15分

--- src/Rational.scala
+++ src/Rational.scala
@@ -3,13 +3,50 @@
   */
 class Rational(n: Int,d: Int) {
   require( d != 0 )
-  val number : Int = n
-  val denom :Int = d
-  override def toString =  n + "/" + d
+  private val g = gcd(n.abs,d.abs)
+  val number : Int = n / g
+  val denom :Int = d / g
+  def this(n: Int) = this(n,1) // 補助コンストラクタ
+  override def toString =  number + "/" + denom
 
-  def add(that:Rational) :Rational =
+  def + (that:Rational) :Rational =
     new Rational(
       number * that.denom + that.number * denom,
       denom * that.denom
     )
+
+  def + (i :Int) :Rational =
+    new Rational(number + 1 * denom ,denom)
+
+  def - (that:Rational) : Rational =
+    new Rational(
+      number * that.denom - that.number * denom,
+      denom * that.denom
+    )
+
+  def - (i: Int) :Rational =
+    new Rational(number - i * denom,denom)
+
+  def * (that:Rational) :Rational =
+    new Rational(number * that.number, denom * that.denom)
+
+  def * (i: Int) : Rational =
+    new Rational(number * i,denom)
+
+  def / (that:Rational) : Rational =
+    new Rational(
+      number * that.denom, denom * that.number
+    )
+
+  def / (i: Int) :Rational =
+    new Rational(number,denom * i)
+
+
+  def lessThan(that:Rational) = this.number * this.denom < that.number * that.denom
+
+  def max(that:Rational) = if((this.lessThan(that)) ) that else this
+
+  private def gcd(a: Int,b:Int) :Int = {
+    if(b==0) a else gcd(b,a%b)
+  }
 }
